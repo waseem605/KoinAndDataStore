7abbd6b8373dcf52faf992ecfbbc686a20d832e2
Waseem Shahzad <w.shahzad605@gmail.com>
2023-01-30 22:37:15 +0500

    version1.0.1

--- app/src/main/java/com/example/koinanddatastore/MainActivity.kt
+++ app/src/main/java/com/example/koinanddatastore/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.koinanddatastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this,"test",Toast.LENGTH_LONG).show()
     }
 }
